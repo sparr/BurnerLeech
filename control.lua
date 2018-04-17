ba9e63325efed94080e25d41581d468d0f6ec2f6
Clarence "Sparr" Risher <sparr0@gmail.com>
2018-04-16 22:15:39 -0700

    WORK IN PROGRESS rebuild inserter list on new mods/versions, optimization, cleanup

--- control.lua
+++ control.lua
@@ -1,13 +1,29 @@
-script.on_event(defines.events.on_tick, function(event)
-	leech()
-end)
+function init_globals()
+  -- [re]build the list of burner/inserter entities
+  global.burner = {}
+  for _,surface in pairs(game.surfaces) do
+    for _,entity in ipairs(surface.find_entities()) do
+      if entity.force.name ~= "neutral" then
+        if string.find(entity.name, "burner") and string.find(entity.name, "inserter") then
+          local burner = entity
+          table.insert(global.burner, burner)
+        end
+      end
+    end
+  end
+  global.burner_index = 1
+  -- [re]build the list of fuel items
+  global.fuel_list = {}
+  for _, proto in pairs (game.item_prototypes) do
+    if proto.fuel_value > 0 then
+      table.insert(global.fuel_list,proto.name)
+    end
+  end
+end
 
 script.on_event(defines.events.on_built_entity, function(event)
 	if not string.find(event.created_entity.name, "burner") then return end
 	if not string.find(event.created_entity.name, "inserter") then return end
-  if not global.burner then
-    global.burner = {}
-  end
   local burner = event.created_entity
   table.insert(global.burner, burner)
   check_burner(burner)
@@ -16,44 +32,22 @@ end)
 script.on_event(defines.events.on_robot_built_entity, function(event)
 	if not string.find(event.created_entity.name, "burner") then return end
 	if not string.find(event.created_entity.name, "inserter") then return end
-  if not global.burner then
-    global.burner = {}
-  end
   local burner = event.created_entity
   table.insert(global.burner, burner)
   check_burner(burner)
 end)
 
 function leech()
-  if not fuel_list then
-  --I recalculate the fuel list everytime because adding or removing mod/items can fuck it all up and this doesn't take very long
-   fuel_list = {}
-    for k, v in pairs (game.item_prototypes) do    
-      if v.fuel_value > 0 then
-        table.insert(fuel_list,v)
-      end
-    end
-  end
-  if global.burner_index == nil then
-  global.burner_index = 1
-  end
-  if global.burner == nil then return end
-  if global.burner[global.burner_index] == nil then return end
-  check_burner(global.burner[global.burner_index])
-  if global.burner_index >= #global.burner then
-    global.burner_index = 1
-  else 
-    global.burner_index = global.burner_index + 1
+  if #global.burner == 0 then return end
+  if check_burner(global.burner[global.burner_index]) then
+    global.burner_index = (global.burner_index % #global.burner) + 1
   end
 end
 
-
 function check_burner(burner)
-  if not burner then return end
-  if not burner.valid then 
-  table.remove(global.burner, global.burner_index) 
-  global.burner_index = global.burner_index -1
-  return
+  if (not burner) or (not burner.valid) then
+    table.remove(global.burner, global.burner_index)
+    return false
   end
   local send_to_target = false
   if burner.drop_target ~= nil then
@@ -63,18 +57,25 @@ function check_burner(burner)
       end
     end
   end
-  if burner.pickup_target == nil then return end
+  if burner.pickup_target == nil then return true end
   if burner.get_item_count() < 1 or send_to_target then
     leeched = burner.pickup_target
     if leeched == nil then return end
-    if burner.held_stack.valid_for_read == false then           
-      for j, v in pairs (fuel_list) do
-        if leeched.get_item_count(v.name) > 0 then                 
-          burner.held_stack.set_stack({name = v.name, count = 1})		
-          leeched.remove_item({name = v.name, count = 1})
-          return
+    if burner.held_stack.valid_for_read == false then
+      for _, fuel in pairs (global.fuel_list) do
+        if leeched.get_item_count(fuel) > 0 then
+          burner.held_stack.set_stack({name = fuel, count = 1})
+          leeched.remove_item({name = fuel, count = 1})
+          return true
         end
       end
     end
   end
+  return true
 end
+
+script.on_event(defines.events.on_tick, leech)
+
+script.on_init(init_globals)
+
+script.on_configuration_changed(init_globals)
